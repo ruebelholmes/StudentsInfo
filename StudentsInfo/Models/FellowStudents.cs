5145934cb19d16ef5a36d99aeb516728c2df0d52
Ruebel Holmes <treyholmes@yahoo.com>
2016-04-14 14:59:15 -0500

    Initial commit

---
+++ StudentsInfo/Models/FellowStudents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentsInfo.Models
+{
+    public class FellowStudents
+    {
+        public string Name { get; set; }
+        public string HairColor { get; set; }
+        public string Height { get; set; }
+
+        public FellowStudents(string name, string haircolor, string height)
+        {
+            Name = name;
+            HairColor = haircolor;
+            Height = height;
+        }
+    }
+}
